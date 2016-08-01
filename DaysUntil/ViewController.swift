650a3023a067a6360667794a74da1d05cf63d672
stevenliao <Stvnliao@yahoo.com>
2016-08-01 19:58:01 -0400

    Initial Commit

---
+++ DaysUntil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DaysUntil
+//
+//  Created by Steven Liao on 8/1/16.
+//  Copyright © 2016 Steven Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
