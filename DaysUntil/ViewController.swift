c75f0a5bf4b3161f29829431448d927e1bafc0e1
stevenliao <Stvnliao@yahoo.com>
2016-08-01 20:25:46 -0400

    Initial commit

--- DaysUntil/ViewController.swift
+++ DaysUntil/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker1: UIDatePicker!
+    @IBOutlet weak var datePicker2: UIDatePicker!
+    @IBOutlet weak var daysApartLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
