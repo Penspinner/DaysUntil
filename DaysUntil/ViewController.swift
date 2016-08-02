6ca18e23b59dbaf142504318246f0fcad212b26f
stevenliao <Stvnliao@yahoo.com>
2016-08-01 21:12:03 -0400

    Finished logic
    Changing either UIDatePicker will update the label

--- DaysUntil/ViewController.swift
+++ DaysUntil/ViewController.swift
@@ -8,21 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
     @IBOutlet weak var datePicker1: UIDatePicker!
     @IBOutlet weak var datePicker2: UIDatePicker!
     @IBOutlet weak var daysApartLabel: UILabel!
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        datePicker1.addTarget(self, action: #selector(updateDaysApartLabel), forControlEvents: .ValueChanged)
+        
+        datePicker2.addTarget(self, action: #selector(updateDaysApartLabel), forControlEvents: .ValueChanged)
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func updateDaysApartLabel()
+    {
+        let date1 = datePicker1.date
+        let date2 = datePicker2.date
+        
+        let calendar = NSCalendar.currentCalendar()
+        let component = calendar.components([.Day], fromDate: date1, toDate: date2, options: [])
+        
+        daysApartLabel.text = "\(abs(component.day)) days apart"
+    }
 }
 
